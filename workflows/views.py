7771a7d6bee8bffd84ac28b6b0e2b7b6203a391f
NGunko <https://NGunko@bitbucket.org>
2018-11-10 14:57:22 +0100

    hola?

--- workflows/views.py
+++ workflows/views.py
@@ -13,7 +13,7 @@ def index(request):
   	category_list = Category.objects.order_by('-likes')[:5]
 	"""page_list = Page.objects.order_by('-views')[:5]"""
 	"""context_dict = {'categories': category_list, 'pages': page_list}"""
-	return render(request, 'workflows/index.html' """, context_dict""")
+	return render(request, 'workflows/index.html')
 
 def about(request):
 	# Construct a dictionary to pass to the template engine as its context.
